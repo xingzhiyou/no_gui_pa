c12446c29c20dcc70c5dda83797396b6240c0dba
xingzhiyou <1043953801@qq.com>
2024-01-03 19:42:02 +0800

    开摆

--- anjukeZu/main.py
+++ anjukeZu/main.py
@@ -15,16 +15,7 @@ class PaWeb:
         house = []
         soup1 = BeautifulSoup(self.open_web(f'https://sh.zu.anjuke.com/fangyuan/p{n}/').text)
         connect = soup1.find_all('div', class_='zu-itemmod clearfix')
-        for connects in connect:
-            name = connects.find_all('b', class_='strongbox')
-            name = name[0].text
-            b_tag = connects.find_all('p', class_='details-item tag')
-            b_tag = b_tag[0].text
-
-            dictionary = {
-                '名字': name,
-            }
-            house.append(dictionary)
+        print(connect)
         print(house)
 
 
